913ce9ecb0f93fb049788bb71a94d9a17e4656aa
Owen <owenkhang@gmail.com>
2021-06-05 18:23:36 +0700

    add aws eb deploy config

---
+++ .platform/hooks/prebuild/02_install_packages.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo 'Run yarn install'
+
+EB_APP_STAGING_DIR=$(sudo /opt/elasticbeanstalk/bin/get-config platformconfig -k AppStagingDir)
+
+cd $EB_APP_STAGING_DIR
+yarn install --production
